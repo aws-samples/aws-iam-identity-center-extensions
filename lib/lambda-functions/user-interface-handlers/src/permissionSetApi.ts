67781a7eb62f5e6d13f80884f84fefd3bb5dfbc7
Leela Lagudu <klagudu@gmail.com>
2022-06-18 09:44:30 +0100

    Add named exception handling to permissionSetAPI handler

--- lib/lambda-functions/user-interface-handlers/src/permissionSetApi.ts
+++ lib/lambda-functions/user-interface-handlers/src/permissionSetApi.ts
@@ -6,24 +6,31 @@
  * - Upsert / delete in permission sets DDB table depending on the operation
  */
 
-// Environment configuration read
 const {
   linksTable,
   AWS_REGION,
   artefactsBucketName,
   DdbTable,
   permissionSetProcessingTopicArn,
+  functionLogMode,
+  AWS_LAMBDA_FUNCTION_NAME,
 } = process.env;
 
-// Lambda types import
-// SDK and third party client imports
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBClient,
+  DynamoDBServiceException,
+} from "@aws-sdk/client-dynamodb";
 import {
   DeleteObjectCommand,
   PutObjectCommand,
   S3Client,
+  S3ServiceException,
 } from "@aws-sdk/client-s3";
-import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import {
+  PublishCommand,
+  SNSClient,
+  SNSServiceException,
+} from "@aws-sdk/client-sns";
 import {
   DynamoDBDocumentClient,
   GetCommand,
@@ -32,7 +39,7 @@ import {
   QueryCommand,
   QueryCommandOutput,
 } from "@aws-sdk/lib-dynamodb";
-//Import validator function and dependencies
+
 import Ajv from "ajv";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import { readFileSync } from "fs";
@@ -43,14 +50,18 @@ import {
   logModes,
   requestStatus,
 } from "../../helpers/src/interfaces";
-//Import helper utilities and interfaces
+
 import {
   imperativeParseJSON,
   JSONParserError,
 } from "../../helpers/src/payload-validator";
 import { v4 as uuidv4 } from "uuid";
-import { logger } from "../../helpers/src/utilities";
-// SDK and third party client object initialistaion
+import {
+  constructExceptionMessage,
+  constructExceptionMessageforLogger,
+  logger,
+} from "../../helpers/src/utilities";
+
 const ddbClientObject = new DynamoDBClient({
   region: AWS_REGION,
   maxAttempts: 2,
@@ -88,35 +99,101 @@ const deleteSchemaDefinition = JSON.parse(
 );
 const deleteValidate = ajv.compile(deleteSchemaDefinition);
 
+const handlerName = AWS_LAMBDA_FUNCTION_NAME + "";
+let permissionSetName = "";
+
 export const handler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> => {
+  const requestId = uuidv4().toString();
+  logger(
+    {
+      handler: handlerName,
+      logMode: logModes.Info,
+      requestId: requestId,
+      status: requestStatus.InProgress,
+      statusMessage: `Permission Set create/update/delete operation started`,
+    },
+    functionLogMode
+  );
   if (event.body !== null && event.body !== undefined) {
     try {
       const body = JSON.parse(event.body);
-      const requestId = uuidv4().toString();
+      logger(
+        {
+          handler: handlerName,
+          logMode: logModes.Debug,
+          requestId: requestId,
+          status: requestStatus.InProgress,
+          statusMessage: `Permission set parsed successfully from message body`,
+        },
+        functionLogMode
+      );
+      logger(
+        {
+          handler: handlerName,
+          logMode: logModes.Info,
+          requestId: requestId,
+          status: requestStatus.InProgress,
+          statusMessage: `Determined permission set operation is determined to be ${body.action}`,
+        },
+        functionLogMode
+      );
       if (body.action === "create" || body.action === "update") {
         const payload: CreateUpdatePermissionSetPayload = imperativeParseJSON(
           event.body,
           createUpdateValidate
         );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Debug,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Permission Set validated successfully against the schema definition for create/update operation`,
+          },
+          functionLogMode
+        );
+        permissionSetName = payload.permissionSetData.permissionSetName;
         await s3clientObject.send(
           new PutObjectCommand({
             Bucket: artefactsBucketName,
-            Key: `permission_sets/${payload.permissionSetData.permissionSetName}.json`,
+            Key: `permission_sets/${permissionSetName}.json`,
             Body: JSON.stringify(payload.permissionSetData),
             ServerSideEncryption: "AES256",
           })
+        );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Info,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Permission Set upsert to S3 successful`,
+            relatedData: permissionSetName,
+          },
+          functionLogMode
         );
         const fetchPermissionSet: GetCommandOutput =
           await ddbDocClientObject.send(
             new GetCommand({
               TableName: DdbTable,
               Key: {
-                permissionSetName: payload.permissionSetData.permissionSetName,
+                permissionSetName: permissionSetName,
               },
             })
           );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Debug,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Did a fetch on solution persistence to determine if we already know about this permission set`,
+            relatedData: permissionSetName,
+          },
+          functionLogMode
+        );
         await ddbDocClientObject.send(
           new PutCommand({
             TableName: DdbTable,
@@ -125,6 +202,17 @@ export const handler = async (
             },
           })
         );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Info,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Permission Set upsert to Dynamo DB successful`,
+            relatedData: permissionSetName,
+          },
+          functionLogMode
+        );
         if (fetchPermissionSet.Item) {
           await snsClientObject.send(
             new PublishCommand({
@@ -132,10 +220,21 @@ export const handler = async (
               Message: JSON.stringify({
                 requestId: requestId,
                 action: "update",
-                permissionSetName: payload.permissionSetData.permissionSetName,
+                permissionSetName: permissionSetName,
                 oldPermissionSetData: fetchPermissionSet.Item,
               }),
             })
+          );
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Debug,
+              requestId: requestId,
+              status: requestStatus.InProgress,
+              statusMessage: `Determined operation is update, posted the payload with upload action to permissionSetProcessorTopic`,
+              relatedData: permissionSetName,
+            },
+            functionLogMode
           );
         } else {
           await snsClientObject.send(
@@ -144,20 +243,34 @@ export const handler = async (
               Message: JSON.stringify({
                 requestId: requestId,
                 action: "create",
-                permissionSetName: payload.permissionSetData.permissionSetName,
+                permissionSetName: permissionSetName,
               }),
             })
           );
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Debug,
+              requestId: requestId,
+              status: requestStatus.InProgress,
+              statusMessage: `Determined operation is create, posted the payload with create action to permissionSetProcessorTopic`,
+              relatedData: permissionSetName,
+            },
+            functionLogMode
+          );
         }
 
-        logger({
-          handler: "userInterface-permissionSetApi",
-          logMode: logModes.Info,
-          relatedData: payload.permissionSetData.permissionSetName,
-          requestId: requestId,
-          status: requestStatus.InProgress,
-          statusMessage: `Permission Set ${payload.action} operation is being processed`,
-        });
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Info,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Completed processing of permission set payload at the interface level`,
+            relatedData: permissionSetName,
+          },
+          functionLogMode
+        );
         return {
           statusCode: 200,
           body: JSON.stringify({
@@ -170,6 +283,17 @@ export const handler = async (
           event.body,
           deleteValidate
         );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Debug,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            statusMessage: `Permission Set validated successfully against the schema definition for delete operation`,
+          },
+          functionLogMode
+        );
+        permissionSetName = payload.permissionSetData.permissionSetName;
         const relatedLinks: QueryCommandOutput = await ddbDocClientObject.send(
           // QueryCommand is a pagniated call, however the logic requires
           // checking only if the result set is greater than 0
@@ -185,16 +309,30 @@ export const handler = async (
             },
           })
         );
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Debug,
+            requestId: requestId,
+            status: requestStatus.InProgress,
+            relatedData: permissionSetName,
+            statusMessage: `Queried if there are any related account assignments`,
+          },
+          functionLogMode
+        );
 
         if (relatedLinks.Items?.length !== 0) {
-          logger({
-            handler: "userInterface-permissionSetApi",
-            logMode: logModes.Warn,
-            relatedData: payload.permissionSetData.permissionSetName,
-            requestId: requestId,
-            status: requestStatus.Aborted,
-            statusMessage: `Permission Set ${payload.action} operation aborted as there are existing account assignments referencing the permission set`,
-          });
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Warn,
+              requestId: requestId,
+              status: requestStatus.Aborted,
+              relatedData: permissionSetName,
+              statusMessage: `Permission set delete operation is aborted as there are existing account assignments referencing this permission set`,
+            },
+            functionLogMode
+          );
           return {
             statusCode: 400,
             body: JSON.stringify({
@@ -203,11 +341,33 @@ export const handler = async (
             }),
           };
         } else {
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Debug,
+              requestId: requestId,
+              status: requestStatus.InProgress,
+              relatedData: permissionSetName,
+              statusMessage: `Determined there are no account assignments referencing this permission set`,
+            },
+            functionLogMode
+          );
           await s3clientObject.send(
             new DeleteObjectCommand({
               Bucket: artefactsBucketName,
               Key: `permission_sets/${payload.permissionSetData.permissionSetName}.json`,
             })
+          );
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Info,
+              requestId: requestId,
+              relatedData: permissionSetName,
+              status: requestStatus.InProgress,
+              statusMessage: `Processed delete in S3`,
+            },
+            functionLogMode
           );
           await snsClientObject.send(
             new PublishCommand({
@@ -219,14 +379,17 @@ export const handler = async (
               }),
             })
           );
-          logger({
-            handler: "userInterface-permissionSetApi",
-            logMode: logModes.Info,
-            relatedData: payload.permissionSetData.permissionSetName,
-            requestId: requestId,
-            status: requestStatus.InProgress,
-            statusMessage: `Permission Set ${payload.action} operation is being processed`,
-          });
+          logger(
+            {
+              handler: handlerName,
+              logMode: logModes.Info,
+              requestId: requestId,
+              relatedData: permissionSetName,
+              status: requestStatus.InProgress,
+              statusMessage: `Sent delete payload to permissionSet processing topic`,
+            },
+            functionLogMode
+          );
           return {
             statusCode: 200,
             body: JSON.stringify({
@@ -236,13 +399,17 @@ export const handler = async (
           };
         }
       } else {
-        logger({
-          handler: "userInterface-permissionSetApi",
-          logMode: logModes.Exception,
-          requestId: requestId,
-          status: requestStatus.FailedWithError,
-          statusMessage: `Permission Set operation failed due to invalid action - ${body.action}`,
-        });
+        logger(
+          {
+            handler: handlerName,
+            logMode: logModes.Exception,
+            requestId: requestId,
+            relatedData: permissionSetName,
+            status: requestStatus.FailedWithException,
+            statusMessage: `Permission Set operation failed due to invalid action - ${body.action}`,
+          },
+          functionLogMode
+        );
         return {
           statusCode: 400,
           body: JSON.stringify({
@@ -254,45 +421,100 @@ export const handler = async (
     } catch (err) {
       if (err instanceof JSONParserError) {
         logger({
-          handler: "userInterface-permissionSetApi",
+          handler: handlerName,
+          requestId: requestId,
           logMode: logModes.Exception,
           status: requestStatus.FailedWithException,
-          statusMessage: `Permission Set operation failed due to schema validation errors:${JSON.stringify(
-            err.errors
-          )}`,
+          statusMessage: constructExceptionMessageforLogger(
+            handlerName,
+            "Schema validation exception",
+            `Provided permission set ${permissionSetName} payload does not pass the schema validation`,
+            JSON.stringify(err.errors)
+          ),
         });
         return {
           statusCode: 500,
-          body: JSON.stringify({ errors: err.errors }),
+          body: constructExceptionMessage(
+            handlerName,
+            "Schema validation exception",
+            `Provided permission set ${permissionSetName} payload does not pass the schema validation`,
+            JSON.stringify(err.errors)
+          ),
+        };
+      } else if (
+        err instanceof S3ServiceException ||
+        err instanceof DynamoDBServiceException ||
+        err instanceof SNSServiceException
+      ) {
+        logger({
+          handler: handlerName,
+          requestId: requestId,
+          logMode: logModes.Exception,
+          status: requestStatus.FailedWithException,
+          statusMessage: constructExceptionMessageforLogger(
+            handlerName,
+            err.name,
+            err.message,
+            permissionSetName
+          ),
+        });
+        return {
+          statusCode: 500,
+          body: constructExceptionMessage(
+            handlerName,
+            err.name,
+            err.message,
+            permissionSetName
+          ),
         };
       } else {
         logger({
-          handler: "userInterface-permissionSetApi",
+          handler: handlerName,
+          requestId: requestId,
           logMode: logModes.Exception,
           status: requestStatus.FailedWithException,
-          statusMessage: `Permission Set operation failed due to exception:${JSON.stringify(
-            err
-          )}`,
+          statusMessage: constructExceptionMessageforLogger(
+            handlerName,
+            "Unhandled exception",
+            JSON.stringify(err),
+            permissionSetName
+          ),
         });
         return {
           statusCode: 500,
           body: JSON.stringify({
-            message: `Exception while processing the call ${err}`,
+            message: constructExceptionMessage(
+              handlerName,
+              "Unhandled exception",
+              JSON.stringify(err),
+              permissionSetName
+            ),
           }),
         };
       }
     }
   } else {
     logger({
-      handler: "userInterface-permissionSetApi",
+      handler: handlerName,
+      requestId: requestId,
       logMode: logModes.Exception,
       status: requestStatus.FailedWithException,
-      statusMessage: `Permission Set operation failed due to invalid message body`,
+      statusMessage: constructExceptionMessageforLogger(
+        handlerName,
+        "Invalid message body exception",
+        "Message body provided is invalid",
+        permissionSetName
+      ),
     });
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: "Invalid message body provided",
+        message: constructExceptionMessage(
+          handlerName,
+          "Invalid message body exception",
+          "Message body provided is invalid",
+          permissionSetName
+        ),
       }),
     };
   }
